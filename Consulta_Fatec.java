f7411c9e235230dd8caaeb1e3a7f20f29a607a79
Adriano José da Silva <adriano.silva@modalgr.com.br>
2023-08-14 18:46:35 -0300

    consulta fatec

---
+++ Consulta_Fatec.java
@@ -0,0 +1,8 @@
+public class Consulta_Fatec {
+public static void main(String[] args) {
+    //variavel de instancia
+    Fatec f = new Fatec ();
+    f.Retorna_unidade("Baixada Santista");
+    f.Retorna_curso("Administração de Sistema - manhã");
+}
+}
