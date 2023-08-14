100d907e737af99883895eb313b3900a8a807139
Adriano José da Silva <adriano.silva@modalgr.com.br>
2023-08-14 19:22:19 -0300

    atividade concessionaria

---
+++ fatec/concessionaria/src/dadosconcessionaria.java
@@ -0,0 +1,20 @@
+package automovel;
+
+public class automovel {
+
+String marca;
+String modelo;
+String cor;
+Double preco;
+
+public void Informa_Automovel () {
+
+System.out.println("MARCA: "+marca);
+
+System.out.println("MODELO: " + modelo);
+
+System.out.println("COR: " + cor);
+ System.out.println("PRECO: " + preco);
+
+}
+}
